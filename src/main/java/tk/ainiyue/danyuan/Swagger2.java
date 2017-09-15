bd20982f1ba492379a86dbfbb7a0bfdfa409b597
514840279 <514840279@qq.com>
2017-09-15 17:21:26 +0800

    修改备注

--- src/main/java/tk/ainiyue/danyuan/Swagger2.java
+++ src/main/java/tk/ainiyue/danyuan/Swagger2.java
@@ -33,15 +33,15 @@ public class Swagger2 {
 		        .paths(PathSelectors.any())
 		        .build();
 	}
-
+	
 	private ApiInfo apiInfo() {
 		return new ApiInfoBuilder()
 		        .title("Spring Boot中使用Swagger2构建RESTful APIs")
-		        .description("更多Spring Boot相关文章请关注：http://blog.didispace.com/")
-		        .termsOfServiceUrl("http://blog.didispace.com/")
-		        .contact("程序猿DD")
+		        .description("详细描述参照源码 https://github.com/514840279/danyuan-application")
+		        .termsOfServiceUrl("http://danyuan.wang")
+		        .contact("")
 		        .version("1.0")
 		        .build();
 	}
-
+	
 }
