75760aefe59c91eaac69251cbba1344e4f0510c0
514840279 <514840279@qq.com>
2019-03-05 14:11:23 +0800

    取消配置改用文件配置，解决jpa失效

--- src/main/java/org/danyuan/application/common/config/MyURLPatternConfiguration.java
+++ src/main/java/org/danyuan/application/common/config/MyURLPatternConfiguration.java
@@ -1,18 +1,16 @@
 package org.danyuan.application.common.config;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
-@Configuration
+//@Configuration
 public class MyURLPatternConfiguration extends WebMvcConfigurationSupport {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		// 项目目录中的file文件夹
 		// 注:如果是Linux的话，直接指定文件夹路径即可，不需要指定哪个盘(Linux就一个可用盘)
 		String dirString = System.getProperty("user.dir");
-		registry.addResourceHandler("/file/**").addResourceLocations("file:" + dirString + "/file/");
-		registry.addResourceHandler("**").addResourceLocations("classpath:/static/");
+		registry.addResourceHandler("*").addResourceLocations("file:" + dirString + "/file/").addResourceLocations("classpath:/META-INF/resources/").addResourceLocations("classpath:/resources/").addResourceLocations("classpath:/static/").addResourceLocations("classpath:/public/");
 		super.addResourceHandlers(registry);
 	}
 	
