812cecdbec60fdd5b7aa1e7d061353eaefc3c0eb
514840279 <514840279@qq.com>
2019-07-16 21:45:24 +0800

    菜单管理修改单选首页

--- src/main/java/org/danyuan/application/softm/sysmenu/dao/SysMenuDao.java
+++ src/main/java/org/danyuan/application/softm/sysmenu/dao/SysMenuDao.java
@@ -16,14 +16,14 @@ import org.springframework.transaction.annotation.Transactional;
 @DynamicUpdate(true)
 @DynamicInsert(true)
 public interface SysMenuDao extends JpaRepository<SysMenuInfo, String> {
-	
+
 	/**
 	 * 方法名： findAllByF_ParentIdOrderByF_SortCode 功 能： 菜单查询 参 数： @param
 	 * F_ParentId 参 数： @return 返 回： List<Authority> 作 者 ： wang @throws
 	 */
 	@Query("select t from SysMenuInfo t where t.parentsId =?1 order by t.sort")
 	public List<SysMenuInfo> findAllByParentsIdOrderByF_SortCode(String parentsId);
-	
+
 	/**
 	 * 方法名 ： findAll 功 能 ： 查询全部数据 参 数 ： @return 参 考 ： @see
 	 * org.springframework.data.repository.CrudRepository#findAll() 作 者 ： wang
@@ -31,14 +31,14 @@ public interface SysMenuDao extends JpaRepository<SysMenuInfo, String> {
 	@Override
 	@Query("select t from SysMenuInfo t  where  t.deleteFlag = 0 order by t.sort")
 	public List<SysMenuInfo> findAll();
-	
+
 	/**
 	 * 方法名： findAllById 功 能：找到一条数据 参 数： @return 返 回： Authority 作 者 ：
 	 * wang @throws
 	 */
 	@Query("select t from SysMenuInfo t  where t.uuid =:uuid")
 	public SysMenuInfo findAllByUuid(@Param("uuid") String uuid);
-	
+
 	/**
 	 * 方法名： updateAuthorityName 功 能： 更新名称 参 数： @param getfFullName 参 数： @param
 	 * getfModuleId 返 回： void 作 者 ： wang @throws
@@ -47,14 +47,14 @@ public interface SysMenuDao extends JpaRepository<SysMenuInfo, String> {
 	@Modifying(clearAutomatically = true)
 	@Query("update SysMenuInfo t  set t.name=:name where t.uuid =:uuid")
 	public void updateSysMenuInfoName(@Param("name") String name, @Param("uuid") String uuid);
-	
+
 	/**
 	 * 方法名： getSize 功 能： TODO(这里用一句话描述这个方法的作用) 参 数： @param getfParentId 参
 	 * 数： @return 返 回： int 作 者 ： wang @throws
 	 */
 	@Query("select t from SysMenuInfo t  where t.parentsId =:parentsId  and t.deleteFlag = 0")
 	public List<SysMenuInfo> getSize(@Param("parentsId") String parentsId);
-	
+
 	/**
 	 * 方法名： updateAuthorityName 功 能： TODO(这里用一句话描述这个方法的作用) 参 数： @param
 	 * getfParentId 参 数： @param i 参 数： @param str 返 回： void 作 者 ： wang @throws
@@ -63,17 +63,17 @@ public interface SysMenuDao extends JpaRepository<SysMenuInfo, String> {
 	@Modifying(clearAutomatically = true)
 	@Query("update SysMenuInfo t  set t.parentsId=:parentsId,t.sort=:sort where t.uuid =:uuid")
 	public void updateSysMenuInfoName(@Param("parentsId") String parentsId, @Param("sort") int sort, @Param("uuid") String uuid);
-	
+
 	/**
 	 * 方法名： getFParentId 功 能： TODO(这里用一句话描述这个方法的作用) 参 数： @param getfParentId 参
 	 * 数： @return 返 回： String 作 者 ： wang @throws
 	 */
 	@Query("select t from SysMenuInfo t  where t.uuid =:uuid  and t.deleteFlag = 0")
 	public SysMenuInfo getParentId(@Param("uuid") String uuid);
-
+	
 	@Query("SELECT DISTINCT t FROM SysMenuInfo t " + "WHERE t.uuid IN(" + "	SELECT a.id.menuId FROM SysRolesJurisdictionInfo a " + "	WHERE a.id.roleId IN ( " + "		SELECT b.rolesId FROM SysUserRolesInfo b " + "		WHERE b.userId IN ( " + "			SELECT uuid FROM SysUserBaseInfo c " + "			WHERE c.userName=:userName " + "		) " + "		AND b.checked='1' " + "	) " + "	AND a.checked='1' " + ") " + "AND t.parentsId = :uuid  and t.deleteFlag = 0 order by t.sort ")
 	public List<SysMenuInfo> findzTreeByUser(@Param("uuid") String id, @Param("userName") String userName);
-
+	
 	/**
 	 * 方法名： updateSysMenuInfoHomePage
 	 * 功 能： TODO(这里用一句话描述这个方法的作用)
@@ -84,9 +84,9 @@ public interface SysMenuDao extends JpaRepository<SysMenuInfo, String> {
 	 */
 	@Transactional
 	@Modifying(clearAutomatically = true)
-	@Query("update SysMenuInfo t set t.homePage=0 where t.homePage=1")
+	@Query("update SysMenuInfo t set t.homePage=0 where t.homePage=1 or t.homePage is null")
 	public void updateSysMenuInfoHomePage();
-
+	
 	/**
 	 * 方法名： findzTreeByUser
 	 * 功 能： TODO(这里用一句话描述这个方法的作用)
@@ -98,5 +98,5 @@ public interface SysMenuDao extends JpaRepository<SysMenuInfo, String> {
 	 */
 	@Query("SELECT DISTINCT t FROM SysMenuInfo t " + "WHERE  t.parentsId = :uuid  order by t.sort ")
 	public List<SysMenuInfo> findzTreeByUser(@Param("uuid") String id);
-
+	
 }
